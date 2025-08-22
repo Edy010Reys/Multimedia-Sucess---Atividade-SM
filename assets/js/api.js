12c2da12e8d1a07267adc1d400421fec9de21689
Edy010Reys <edson10vasconcelos@gmail.com>
2025-08-22 18:55:10 -0300

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 const obterDadosDoPerfil = async () => {
-    const url = 'https://github.com/Edy010Reys/portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Edy010Reys/portfolio/main/data/profile.json';
     const resposta = await fetch(url);
     return resposta.json();
 };
